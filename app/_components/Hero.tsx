99b09f4409d3765edf87fb2e5180d0a605b6764b
Esteban <baab2an@live.fr>
2024-06-01 11:56:54 +0200

    FEAT infos hero text

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,16 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
+
+import { cn } from "@/lib/utils";
+import { ComponentPropsWithoutRef } from "react";
 import { Section } from "./Section";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
+const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
+  return (
+    <span
+      className={cn(
+        "bg-accent/30 font-mono border hover:bg-accent/50 border-accent px-1 py-0.5 text-primary rounded-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+};
 
 export const Hero = () => {
   return (
     <>
       <Section className="flex max-lg:flex-col items-start">
         <div className="flex-[2]">
-          <h2 className="font-caption text-5xl">Esteban Lopez</h2>
-          <h3>Software developer and creator</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            similique doloremque enim excepturi qui consequatur?
+          <h2 className="font-caption font-bold text-5xl text-primary">
+            Esteban Lopez
+          </h2>
+          <h3 className="text-3xl font-caption">
+            Software developer and creator
+          </h3>
+          <p className="text-base">
+            I love creating content on{" "}
+            <Code className="inline-flex items-center gap-1">
+              <LinkedinIcon size={16} className="inline" />
+              Linkedin
+            </Code>
           </p>
         </div>
         <div className="flex-1">
