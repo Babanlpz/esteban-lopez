c2298a1af1a6017c7f9c301ca9f28aee01ac3a20
Esteban <baab2an@live.fr>
2024-06-02 10:33:33 +0200

    FEAT section me contacter

--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -2,6 +2,8 @@
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import {
+  ALargeSmall,
+  ArrowUpRight,
   Code,
   LucideIcon,
   MessageCircle,
@@ -40,9 +42,57 @@ export const Status = () => {
             ))}
           </div>
         </Card>
-        <Card className="p-4 flex-1">Contact me</Card>
+        <Card className="p-4 flex-1 flex flex-col gap-2">
+          <p className="text-lg text-muted-foreground">Contact me</p>
+          <ContactCard
+            name="@estebanlopezdev"
+            image="/esteban.png"
+            mediumImage="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+            description="256"
+          />
+          <ContactCard
+            name="@estebanlopez"
+            image="/esteban.png"
+            mediumImage="https://play-lh.googleusercontent.com/pCYfqB1v-c9Phmvjxji_v37Lf1l59fg1pIy17PztwmS2yoRvGtNAgHZlOHQf0AQo_MZv"
+            description="1"
+          />
+        </Card>
       </div>
     </Section>
+  );
+};
+
+const ContactCard = (props: {
+  image: string;
+  mediumImage: string;
+  name: string;
+  description: string;
+}) => {
+  return (
+    <Card className="p-3 bg-accent/10 hover:bg-accent/30 transition-colors group flex items-center gap-4">
+      <div className="relative">
+        <img
+          src={props.image}
+          alt={props.name}
+          className="w-10 h-10 rounded-full object-contain"
+        />
+        <img
+          src={props.mediumImage}
+          alt={props.name}
+          className="w-4 h-4 absolute -bottom-1 -right-1 rounded-full object-contain"
+        />
+      </div>
+      <div className="mr-auto">
+        <div className="flex items-center gap-2">
+          <p className="text-lg font-semibold">{props.name}</p>
+        </div>
+        <p className="text-xs text-muted-foreground">{props.description}</p>
+      </div>
+      <ArrowUpRight
+        className="group-hover:translate-x-2 mr-4 groupe-hover:-translate-y-2 transition-transform"
+        size={16}
+      />
+    </Card>
   );
 };
 
@@ -73,6 +123,12 @@ const SIDE_PROJECTS: SideProjectProps[] = [
   },
   {
     Logo: Weight,
+    title: "Esteban Lopez",
+    description: "Software developer and creator",
+    url: "/",
+  },
+  {
+    Logo: ALargeSmall,
     title: "Esteban Lopez",
     description: "Software developer and creator",
     url: "/",
